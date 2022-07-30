01f7b50ec4110d73df796370fe0413068cf49a8b
Samuel Ramon <samuelrbo@gmail.com>
2022-07-30 11:20:16 +0100

    fix(gateway): 🚑 Fix Project MS client config

--- api-gateway/src/modules/shared/shared.module.ts
+++ api-gateway/src/modules/shared/shared.module.ts
@@ -2,8 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
 import { AuthClient } from '@clients/auth.client';
 import { UserClient } from '@clients/user.client';
+import { ProjectClient } from '@clients/project.client';
 
-const clients = ClientsModule.register([AuthClient, UserClient]);
+const clients = ClientsModule.register([AuthClient, UserClient, ProjectClient]);
 
 @Global()
 @Module({
